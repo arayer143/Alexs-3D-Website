a954145d0bbfdd8486d047db83fe064b4c4e3b4f
arayer143 <89434892+arayer143@users.noreply.github.com>
2023-12-23 12:26:54 -0600

    another small change

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,9 @@ import "./index.css";
 
 
 
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
     <App />
-
+  </React.StrictMode>
 );
