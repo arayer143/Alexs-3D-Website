65d923721a601745323e9411e3b9b4b91ad925d0
arayer143 <89434892+arayer143@users.noreply.github.com>
2023-12-23 15:48:48 -0600

    change

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -21,6 +21,9 @@ module.exports = {
       backgroundImage: {
         "hero-pattern": "url('/src/assets/herobg.png')",
       },
+      logo: {
+        "logo": "url('/public/logo-color.png')",
+      },
     },
   },
   plugins: [],
